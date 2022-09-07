991a3d4e7e5fb449348632ebe34c3fb87b8aeb56
toutsos <a.a.toutsios@army.gr>
2022-09-07 14:32:54 +0300

    tut - end: employee update included

--- src/main/java/com/toutsos/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/toutsos/springboot/repository/EmployeeRepository.java
@@ -4,6 +4,10 @@ package com.toutsos.springboot.repository;
 import com.toutsos.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+
+    Optional<Employee> findByEmail(String email);
 
 }
--- src/test/java/com/toutsos/springboot/repository/EmployeeRepositoryTests.java
+++ src/test/java/com/toutsos/springboot/repository/EmployeeRepositoryTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 public class EmployeeRepositoryTests {
 
@@ -31,4 +33,88 @@ public class EmployeeRepositoryTests {
         assertThat(savedEmployee.getId()).isGreaterThan(0);
     }
 
+    //JUnit test for get all employees operaion
+    @Test
+    public void givenEmployeesList_whenFindAll_thenEmployeesList(){
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Ramesh")
+                .email("ramesh@gmail.com")
+                .build();
+
+        Employee employee1 = Employee.builder()
+                .firstName("John")
+                .lastName("Cena")
+                .email("cena@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+        employeeRepository.save(employee1);
+
+        //when - action or the behaviour that we are going test
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        //then - verify the output
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
+    }
+
+    //JUnit test for get Employee by Id operation
+    @Test
+    public void givenEmployeeObject_whenFindById_thenReturnTheEmployeeObject(){
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Ramesh")
+                .email("ramesh@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+
+        //when - action or the behaviour that we are going test
+        Employee employeeDB = employeeRepository.findById(employee.getId()).get();
+
+        //then - verify the output
+        assertThat(employeeDB).isNotNull();
+    }
+
+    //JUnit test for get employee by email
+    @Test
+    public void givenEmployeeEmail_whenFindByEmail_thenReturnEmployeeObject(){
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Ramesh")
+                .email("ramesh@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+
+        //when - action or the behaviour that we are going test
+        Employee employeeDB = employeeRepository.findByEmail(employee.getEmail()).get();
+
+        //then - verify the output
+        assertThat(employeeDB).isNotNull();
+    }
+
+    //JUnit test for update Employee operation
+    @Test
+    public void givenEmployeeObject_whenUpdateEmployee_thenReturnUpdatedEmployee(){
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Ramesh")
+                .email("ramesh@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+
+        //when - action or the behaviour that we are going test
+
+        Employee employeeDB = employeeRepository.findById(employee.getId()).get();
+        employeeDB.setEmail("angelos.toutsios@gmail.com");
+        employeeDB.setFirstName("Angelos");
+        Employee updatedEmployee = employeeRepository.save(employeeDB);
+
+        //then - verify the output
+        assertThat(updatedEmployee.getEmail()).isEqualTo("angelos.toutsios@gmail.com");
+        assertThat(updatedEmployee.getFirstName()).isEqualTo("Angelos");
+    }
 }
