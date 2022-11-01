e1a1c9ea5e7f09a2c665d3752cec0812eb214c12
angel <angelos.toutsios@gmail.com>
2022-11-01 12:56:34 +0200

    tut 40 finish

---
+++ src/main/java/com/toutsos/springboot/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.toutsos.springboot.controller;
+
+import com.toutsos.springboot.model.Employee;
+import com.toutsos.springboot.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+
+
+
+}
